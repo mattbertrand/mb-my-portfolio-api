4d68ff72f739e7aaa5910e4c0c4150ab33ef8e4c
github username <github email address>
2021-04-08 16:04:20 -0700

    added some seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(title: "California Wineries", category: "A rails application", image_link: "https://miro.medium.com/max/2000/1*u_yfyxB8r_mwAhgUgq-kFQ.png")
+Project.create(title: "Restaurant Style", category: "Javascript Single Page Application", image_link: "https://miro.medium.com/max/1400/1*0ht-MhUCkMpws-97uFk6Dw.png")
+Project.create(title: "Wine Library", category: "Sinatra Project", image_link: "https://miro.medium.com/max/2000/1*gWq0KTYZx2Xmhe7u4hO3pg.png")

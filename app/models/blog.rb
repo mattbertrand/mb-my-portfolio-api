a8b1b7e22ec34fe756d9cedc21f9d42a63363adb
github username <github email address>
2021-04-11 11:50:26 -0700

    added association and custom setter method

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,7 @@
 class Blog < ApplicationRecord
+    belongs_to :project
+
+    def project_attributes=(project_title)
+        self.project = Project.find_or_create_by(title: project_title)
+    end
 end

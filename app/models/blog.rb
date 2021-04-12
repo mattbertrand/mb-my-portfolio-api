c0cefde195d5c89f05b66bb6fdf4fbf09ea63c60
github username <github email address>
2021-04-12 13:40:05 -0700

    removed resource blog using rails d scaffold generator

--- app/models/blog.rb
+++
@@ -1,7 +0,0 @@
-class Blog < ApplicationRecord
-    belongs_to :project
-
-    def project_attributes=(project_title)
-        self.project = Project.find_or_create_by(title: project_title)
-    end
-end

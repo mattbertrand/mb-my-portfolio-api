0672c772d413f91fecd64d5adb080b45037952f4
github username <github email address>
2021-04-12 13:46:58 -0700

    removed blogs table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_175819) do
+ActiveRecord::Schema.define(version: 2021_04_12_204346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blogs", force: :cascade do |t|
-    t.text "content"
-    t.integer "project_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"

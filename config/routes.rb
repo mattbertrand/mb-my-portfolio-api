15df0c0fd047129556fdb1b74aab8fe14488a289
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-04-15 09:46:22 -0700

    added a sessions controller using rails generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :projects
+
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

ae40cb8c95f6804fcf389cc6b82af8e464c5027c
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-04-15 16:01:28 -0700

    updated routes

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_162656) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
